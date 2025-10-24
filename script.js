e172b4b8af0cd42e3c17249fb9c0b677a16e2b8c
mohdalitech <mohdalii1923@gmail.com>
2025-10-25 01:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function firsttask(){
     task1.style.color="green";
 }
 function Yes(){
-    alert("Your positive response is appreciared!");
+    alert("Your positive response is appreciated!");
     let btn1=document.getElementById("choice1");
     let btn2=document.getElementById("choice2");
     btn1.disabled=true;
@@ -21,4 +21,27 @@ function No(){
     alert(`Very sorry for you ${name},for your reason ${reason} of not responding to greeting..`);
     btn1.disabled=true;
     btn2.disabled=true;
+}
+function mood(){
+let mood_1=document.getElementById("select").value;
+let image=document.getElementById("image");
+let video=document.getElementById("video")
+if(mood_1==="Calm🧘"){
+    image.src="calm.png";
+    video.src="https://www.youtube.com/embed/ZToicYcHIOU?si=6KgYaOXmPLd3mav-"; 
+}
+if(mood_1==="Busy🧠"){
+    image.src="busy.png";
+    video.src="https://www.youtube.com/embed/HGn5XJtwmAY?si=HhCf6wD82aZkG433";
+}
+if(mood_1==="Overthinking😵")
+{
+    image.src="overthink.png";
+    video.src="https://www.youtube.com/embed/m1RGFegL83c?si=EFbXtB7dC9_n9KI4";
+}
+if(mood_1==="Peaceful🌿")
+{
+    image.src="peace.png";
+    video.src="https://www.youtube.com/embed/KLuTLF3x9sA?si=JQu2HijlVatpGxA6";
+}
 }
